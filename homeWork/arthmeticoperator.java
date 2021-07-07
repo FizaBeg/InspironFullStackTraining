848df24fc3451ad18df95e35b1dfe555e97ba501
FizaBeg <69510055+FizaBeg@users.noreply.github.com>
2021-07-07 16:18:05 +0530

    Add files via upload

---
+++ homeWork/arthmeticoperator.java
@@ -0,0 +1,19 @@
+package homeWork;
+
+public class arthmeticoperator {
+	public static void main(String[] args) {
+		
+		int i=100;
+		i-=10;   // i=i-10
+		System.out.println(i);  //print 90
+		
+		int j=50;
+		j/=5;    //j=j/5
+		System.out.println(j);  //print 10
+		
+		int k=15;
+		k%=2;    //k=k%2
+		System.out.println(k);  //print 1
+	}
+
+}
---
+++ homeWork/decrementOp.java
@@ -0,0 +1,19 @@
+package homeWork;
+
+public class decrementOp {
+	public static void main(String[] args) {
+		int i=100;
+		
+		System.out.println(i); //print 100
+		System.out.println(i--);  //print 100 and then decrement  
+		System.out.println(i);    //print 99 as decremented
+		System.out.println();
+		
+		int s= 20;
+		
+		System.out.println(s);    //print 20
+		System.out.println(--s);  //decremnet first then print 20
+		System.out.println(s);    //print 20
+		
+	}
+}
