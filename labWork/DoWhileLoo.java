497474f2e20b74aa1f7c7c5f6a697c4ef5ad90fd
FizaBeg <69510055+FizaBeg@users.noreply.github.com>
2021-07-07 16:08:52 +0530

    Add files via upload

---
+++ labWork/DoWhileLoo.java
@@ -0,0 +1,14 @@
+package labWork;
+
+public class DoWhileLoo {
+	public static void main(String args[]) { 
+		 int x = 1; 
+		 
+		 do { 
+		 System.out.print("value of x : " + x ); 
+		 x++; 
+		 System.out.print("\n"); 
+		 }while( x < 11 ); 
+		 } 
+
+}
---
+++ labWork/HelloWorld.java
@@ -0,0 +1,7 @@
+package labWork;
+
+public class HelloWorld {
+ public static void main(String[] args) {
+	System.out.println("Welcome to Java - Inspiron Batch...It is conducted on Monday,Wednesday,Firday at timing 9:30-11:30... All the BEST...");
+}
+}
